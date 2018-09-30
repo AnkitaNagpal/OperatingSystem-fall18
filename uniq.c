6529562f4342f1781cb07ddc534f8d6a31cce392
AnkitaNagpal <30566305+AnkitaNagpal@users.noreply.github.com>
2018-09-30 11:05:45 -0400

    Update uniq.c

--- uniq.c
+++ uniq.c
@@ -4,9 +4,10 @@
 
 
 char buf[512];
-
-char
-toUpperCase (char input)
+int flagI=0;
+int flagD=0;
+int flagC=0;
+char toUpperCase (char input)
 {
   if (input >= 'a' && input <= 'z')
     input = input - 32;
@@ -14,11 +15,7 @@ toUpperCase (char input)
   return input;
 }
 
-
-
-
-void
-uniq (int fd, char *name)
+void uniq (int fd,char flag[])
 {
   int i, n;			//n means the size of the chunk read, i keeps track of index of the chunk
   int l, c;			// l means line number, c means character count in string
@@ -27,114 +24,135 @@ uniq (int fd, char *name)
   int savedStrCounter = 0;
   int savedStrCounterEnd = 0;
   int printFlag = 1;
+  int RepeatCount=1;
   l = c = 0;
   //int lines = 10;
 
   char string[512];
+  if ((strcmp(flag,"-c")==0))
+      flagC=1;
 
 
   while ((n = read (fd, buf, sizeof (buf))) > 0)
     {
 
       for (i = 0; i < n; i++)
-	{
-
-	  if (buf[i] == '\n')
-	    {
-		memset (strRepeat, 0, 512);
-	      strcpy (strRepeat, new_string);	// dont do coping the string pointer
-	      if (printFlag == 0)
 		{
-		  printf (1, "%s\n", new_string);
-		  //write new_string to file  with the escape charachter
-		  printFlag = 1;
-		}
-	      l++;		//Increment line upon new line char
-		
-		memset (new_string, 0, 512);
-	      memset (string, 0, 512);
-	      savedStrCounterEnd = c;
-	      savedStrCounter = 0;
-	      c = 0;
-	    }
-	  else
-	    {
 
-	      if (printFlag == 1)
-		{
-		  if (savedStrCounterEnd >= savedStrCounter)
-		    {
-		      if (buf[i] != strRepeat[savedStrCounter])
+		  if (buf[i] == '\n')
 			{
-			  printFlag = 0;
-			}
-		    }
-		  else
-		    {
-		      printFlag = 0;
-		    }
-		}
-	      new_string[c] = buf[i];
-		savedStrCounter++;
-	      c++;
-	    }
-	  //  if (l == lines) {
-	  //      exit();
-	  // }
-	}
-      /* while ((n = read(fd, buf, sizeof(buf))) > 0) {
-         for (i = 0; i < n; i++) {
-         if (buf[i] == '\n') {
-         l++; //Increment line upon new line char
-         printf(1, "%s\n", string);
-         memset(string, 0, 512);
-         c = 0;
-         }
-         else {
-         string[c] = buf[i];
-         c++;
-         }
-         if (l == lines) {
-         exit();
-         }
-         }
-         } */
+				if (printFlag == 0)
+				{
+					if(RepeatCount>1 && flagC==1)
+					{
+						printf(1,"%d %s\n",RepeatCount,strRepeat);
+					}
+					else if(flagD==1 && RepeatCount>1 )
+					{
+						printf (1, "%d %s\n", RepeatCount,new_string);
 
+					}
+					else{
+						printf (1, "%s\n", new_string);
+					}
+					  
+					memset (strRepeat, 0, 512);
+					strcpy (strRepeat, new_string);
+					  //write new_string to file  with the escape charachter
+					printFlag = 1;
+					RepeatCount=1;
+				}else{
+					RepeatCount++;
+				}
+			  l++;		//Increment line upon new line char
+			
+				  memset (new_string, 0, 512);
+				  memset (string, 0, 512);
+				  savedStrCounterEnd = c;
+				  savedStrCounter = 0;
+				  c = 0;
+			}
+		  else
+			{
+
+			  if (printFlag == 1)
+				{
+				  if (savedStrCounterEnd >= savedStrCounter)
+					{
+					
+						if(flagI==1)
+						{
+							if (toUpperCase(buf[i]) != toUpperCase(strRepeat[savedStrCounter]))
+							{
+								RepeatCount=0;
+								printFlag = 0;
+							}
+						}else{
+							if (buf[i] != strRepeat[savedStrCounter])
+							{
+								RepeatCount=0;
+								printFlag = 0;
+							}
+						}
+					}
+				  else
+					{
+						RepeatCount=0;
+						printFlag = 0;
+					}
+				}
+			    new_string[c] = buf[i];
+				savedStrCounter++;
+			    c++;
+			}
+		
+		}
+		
+		
+		//Check if this is required
+		if(RepeatCount>1 && flagC==1)
+		{
+			printf(1,"%d %s\n",RepeatCount,strRepeat);
+		}
+		else if(flagD==1 && RepeatCount>1 )
+		{
+			printf (1, "%d %s\n", RepeatCount,new_string);
+
+		}
+		
     }
 }
   int main (int argc, char *argv[])
   {
-    int fd,i;//,lines=0;
+    int fd,i;
     char *name;
 
-    fd = 0;			//default, no file specified, so takes user input instead
-    name = "";			//default, assuming no file name specified
+    fd = 0;			
+    name = "";			
 
     if (argc <= 1)
-      {
-	uniq (fd,"textfile.txt");
-	exit ();
-      }
+    {
+		uniq (fd,"0");
+		exit ();
+    }
   else {
             for (i = 1; i < argc; i++) {
                   if (atoi(argv[i]) == 0 && *argv[i] != '0' && *argv[i] != '-') {//executes if not a number
-                        if ((fd = open(argv[i], 0)) < 0) {//executes if can't open the file
-                        //If fd == 0, end of file, if fd == -1, error
+                        if ((fd = open(argv[i], 0)) < 0) {
                         printf(1, "uniq: cannot open %s\n", argv[i]);
                         exit();
                         }
-                        //name = argv[i];
+                  } 
+                  else  if(strcmp(argv[1],"-c")==0){
+					flagC=1;
+					uniq(fd,"-c");
+					exit();                  }
+				  else  if(strcmp(argv[1],"-i")==0){
+					flagI=1;
                   }
-                  else {//it's a number
-			//*argv[i] = 1;
-			argv[i]++;
-                        
-                    //lines= atoi(argv[i]++);
+				  else  if(strcmp(argv[1],"-d")==0){
+					flagD=1;
                   }
-/*
-                  uniq(fd, name, lines);
-                  close(fd);
-*/
             }
            uniq(fd, name);
             close(fd);
